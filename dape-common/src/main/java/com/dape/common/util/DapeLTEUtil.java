35fb022de20b01e743f42ffc2595d759a6a79fe1
DP <xushifu@live.cn>
2018-11-22 22:20:23 +0800

    修改公网连接

--- dape-common/src/main/java/com/dape/common/util/DapeLTEUtil.java
+++ dape-common/src/main/java/com/dape/common/util/DapeLTEUtil.java
@@ -22,16 +22,16 @@ public class DapeLTEUtil implements InitializingBean, ServletContextAware {
 
     @Override
     public void setServletContext(ServletContext servletContext) {
-        LOGGER.info("===== 开始解压dape-admin =====");
-        String version = PropertiesFileUtil.getInstance("dape-lte-client").get("dape.admin.version");
-        LOGGER.info("dape-admin.jar 版本: {}", version);
-        String jarPath = servletContext.getRealPath("/WEB-INF/lib/dape-lte-" + version + ".jar");
-        LOGGER.info("dape-admin.jar 包路径: {}", jarPath);
-        String resources = servletContext.getRealPath("/") + "/resources/dape-lte";
-
-        LOGGER.info("dape-admin.jar 解压到: {}", resources);
-        JarUtil.decompress(jarPath, resources);
-        LOGGER.info("===== 解压dape-admin完成 =====");
+//        LOGGER.info("===== 开始解压dape-admin =====");
+//        String version = PropertiesFileUtil.getInstance("dape-lte-client").get("dape.admin.version");
+//        LOGGER.info("dape-admin.jar 版本: {}", version);
+//        String jarPath = servletContext.getRealPath("/WEB-INF/lib/dape-lte-" + version + ".jar");
+//        LOGGER.info("dape-admin.jar 包路径: {}", jarPath);
+//        String resources = servletContext.getRealPath("/") + "/resources/dape-lte";
+//
+//        LOGGER.info("dape-admin.jar 解压到: {}", resources);
+//        JarUtil.decompress(jarPath, resources);
+//        LOGGER.info("===== 解压dape-admin完成 =====");
     }
 
 }
