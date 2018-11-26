49f123bf6e8605016d6f19cf4428bd5fa904167a
ty3277 <327734933@163.com>
2018-11-26 14:01:01 +0800

    增加分享图片合成，二维码工具类，调整邀请好友分享样式

---
+++ dape-common/src/main/java/com/dape/common/util/QRCodeUtil.java
@@ -0,0 +1,73 @@
+package com.dape.common.util;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.common.BitMatrix;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+import java.util.UUID;
+
+public class QRCodeUtil {
+
+
+    private static final int BLACK = 0xff000000;
+    private static final int WHITE = 0xFFFFFFFF;
+    public static void encode(String contents, File file,String filePostfix, BarcodeFormat format, int width, int height, Map<EncodeHintType, ?> hints) {
+        try {
+            contents = new String(contents.getBytes("UTF-8"), "ISO-8859-1");
+            BitMatrix bitMatrix = new MultiFormatWriter().encode(contents, format, width, height);
+            writeToFile(bitMatrix, filePostfix, file);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void writeToFile(BitMatrix matrix, String format, File file) throws IOException {
+        BufferedImage image = toBufferedImage(matrix);
+        ImageIO.write(image, format, file);
+    }
+    public static BufferedImage toBufferedImage(BitMatrix matrix) {
+        int width = matrix.getWidth();
+        int height = matrix.getHeight();
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                image.setRGB(x, y, matrix.get(x, y) == true ? BLACK : WHITE);
+            }
+        }
+        return image;
+    }
+
+    public static BufferedImage encodeToBufferedImage(String contents, BarcodeFormat format, int width, int height){
+        try {
+            contents = new String(contents.getBytes("UTF-8"), "ISO-8859-1");
+            BitMatrix bitMatrix = new MultiFormatWriter().encode(contents, format, width, height);
+            BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    image.setRGB(x, y, bitMatrix.get(x, y) == true ? BLACK : WHITE);
+                }
+            }
+            return image;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) throws IOException {
+        String strJson = "http://www.163.cpm/";
+        String path = "D:\\";
+        String filePostfix = "jpg";
+        String uuid = UUID.randomUUID().toString();
+        File file = new File(path  +uuid + "."+filePostfix);
+//        file.createNewFile();
+        QRCodeUtil.encode(strJson, file,filePostfix, BarcodeFormat.QR_CODE, 200, 200, null);
+        System.out.println(uuid+".png");
+    }
+
+}
--- dape-shop/dape-shop-web/src/main/java/com/dape/shop/web/controller/IndexController.java
+++ dape-shop/dape-shop-web/src/main/java/com/dape/shop/web/controller/IndexController.java
@@ -1,11 +1,14 @@
 package com.dape.shop.web.controller;
 
 import com.dape.common.base.BaseController;
+import com.dape.common.util.NewImageUtil;
+import com.dape.common.util.StringUtil;
 import com.dape.shop.dao.model.*;
 import com.dape.shop.rpc.api.ShopGoodsService;
 import com.dape.shop.rpc.api.ShopMenuService;
 import com.dape.shop.rpc.api.ShopModuleService;
 import com.dape.shop.rpc.api.ShopUserService;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +17,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 /**
@@ -80,5 +88,85 @@ public class IndexController extends BaseController {
     @RequestMapping(value = "/order", method = RequestMethod.GET)
     public String order(Model model) {
         return thymeleaf("/all_orders");
+    }
+
+    @RequestMapping(value = "/fenxiang", method = RequestMethod.GET)
+    public void fenxiang(Model model, HttpServletRequest request, HttpServletResponse response) {
+        Object o = request.getSession().getAttribute("user");
+        ShopUser user = (ShopUser)o;
+
+        String openId = user.getOpenId();
+        // 项目根路径，绝对路径
+        String proPath = request.getSession().getServletContext().getRealPath("");
+        // 保存到本地的临时头像文件
+        String headTemp = proPath + "/resources/images/"+openId+".tmp";
+        // 生成的分享图片
+        String targetImg = null;
+
+        String ip = "192.168.10.205";
+        int port = request.getLocalPort();
+        // 分享二维码访问地址
+        String qrCode = "http://" + ip + ":" + port;
+
+        // 当前session存在分享图片，直接转出到前端，不存在创建分享图片
+        Object fenxiangImg = request.getSession().getAttribute("fenxiangImg");
+        if(fenxiangImg == null){
+            targetImg = proPath + "/resources/images/"+openId+".jpg";
+            try {
+                // 网络头像保存到本地
+                String headUrl = null;
+                if(StringUtils.isNotEmpty(user.getHeadUrl())){
+                    headUrl = user.getHeadUrl();
+                }else{
+                    String appName = request.getAttribute("appName").toString();
+                    String uiPath = request.getAttribute("uiPath").toString();
+                    headUrl = uiPath + appName + "/images/headimg.jpg";
+                }
+                URL url = new URL(headUrl);
+                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.setConnectTimeout(5 * 1000);
+                InputStream inStream = conn.getInputStream();
+
+                File headF = new File(headTemp);
+                if(!headF.exists()){
+                    headF.createNewFile();
+                }
+                FileOutputStream outHead = new FileOutputStream(headTemp);
+                byte[] buffer = new byte[1024];
+                int len = 0;
+                while( (len=inStream.read(buffer)) != -1 ){
+                    outHead.write(buffer, 0, len);
+                }
+                outHead.close();
+                inStream.close();
+
+                NewImageUtil.fenxiangImg(user.getWeiNickName() , targetImg, proPath+"/resources/images/fenxiang_base.jpg",headTemp,qrCode);
+                headF.delete();
+
+                request.getSession().setAttribute("fenxiangImg", targetImg);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else {
+            targetImg = fenxiangImg.toString();
+        }
+
+        try {
+            FileInputStream inputStream = new FileInputStream(targetImg);
+            int length = inputStream.available();
+            byte data[] = new byte[length];
+            response.setContentLength(length);
+            response.setContentType("image/jpeg");
+            inputStream.read(data);
+            OutputStream out = response.getOutputStream();
+            out.write(data);
+            out.flush();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
