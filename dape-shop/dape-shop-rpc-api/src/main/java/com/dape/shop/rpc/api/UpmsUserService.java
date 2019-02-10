faef30cec44ef8ddfaff903d0229f6b6a888e528
ty3277 <327734933@163.com>
2019-02-11 06:47:59 +0800

    调整订单页样式，时间戳格式化

---
+++ dape-shop/dape-shop-rpc-api/src/main/java/com/dape/shop/rpc/api/UpmsUserService.java
@@ -0,0 +1,13 @@
+package com.dape.shop.rpc.api;
+
+import com.dape.common.base.BaseService;
+import com.dape.shop.dao.model.UpmsUser;
+import com.dape.shop.dao.model.UpmsUserExample;
+
+/**
+* UpmsUserService接口
+* Created by dape on 2019/2/10.
+*/
+public interface UpmsUserService extends BaseService<UpmsUser, UpmsUserExample> {
+
+}
