dca2968e5c7d8fc11d55fb14414fbcdfce5d65c7
ty3277 <327734933@163.com>
2019-02-17 00:30:27 +0800

    搜索页修改

--- dape-ui/dape-shop-web/js/common.js
+++ dape-ui/dape-shop-web/js/common.js
@@ -265,3 +265,336 @@ function formatCouponInfo(info){
         }
     }
 }
+
+// 综合排序
+function sortTTSFnc(){
+    var sort = $('#sort').val();
+    if(sort == 'tk_total_sales_des'){
+      sort = 'tk_total_sales_asc';
+    }else if(sort == 'tk_total_sales_asc'){
+      sort = 'tk_total_sales_des';
+    }else{
+      sort = 'tk_total_sales_des';
+    }
+    $('#sort').val(sort);
+    $('#searchForm').submit();
+}
+// 销量排序
+function sortTSFnc(){
+    var sort = $('#sort').val();
+    if(sort == 'total_sales_des'){
+      sort = 'total_sales_asc';
+    }else if(sort == 'total_sales_asc'){
+      sort = 'total_sales_des';
+    }else{
+      sort = 'total_sales_des';
+    }
+    $('#sort').val(sort);
+    $('#searchForm').submit();
+}
+// 价格排序
+function sortPFnc(){
+    var sort = $('#sort').val();
+    if(sort == 'price_asc'){
+      sort = 'price_des';
+    }else if(sort == 'price_des'){
+      sort = 'price_asc';
+    }else{
+      sort = 'price_asc';
+    }
+    $('#sort').val(sort);
+    $('#searchForm').submit();
+}
+// 设置排序箭头
+function setJian(){
+  var sort = $('#sort').val();
+  if(sort == 'tk_total_sales_des'){
+    $('#tk_total_sales').append('<i class="icon18-zz icon-jian-18-2" style="margin-bottom:-3px;"></i>');
+    $('#tk_total_sales').addClass('on');
+  }else if(sort == 'tk_total_sales_asc'){
+    $('#tk_total_sales').append('<i class="icon18-zz icon-jian-18-2" style="margin-bottom:-3px;"></i>');
+  }else if(sort == 'total_sales_des'){
+    $('#total_sales').append('<i class="icon18-zz icon-jian-18-2" style="margin-bottom:-3px;"></i>');
+    $('#total_sales').addClass('on');
+  }else if(sort == 'total_sales_asc'){
+    $('#total_sales').append('<i class="icon18-zz icon-jian-18-2" style="margin-bottom:-3px;"></i>');
+  }else if(sort == 'price_des'){
+    $('#price_asc').append('<i class="icon18-zz icon-jian-18-2" style="margin-bottom:-3px;"></i>');
+    $('#price_asc').addClass('on');
+  }else if(sort == 'price_asc'){
+    $('#price_asc').append('<i class="icon18-zz icon-jian-18-2" style="margin-bottom:-3px;"></i>');
+  }
+}
+// 首页加载商品
+function loadGoodsForMain(){
+    var platform = $('#platform').val();
+    //$.post('/goods/loadGoods',{pageNum:pageNum,pageSize:pageSize,cat:'16,18',platform:platform,has_coupon:true,need_free_shipment:true,need_prepay:true,material_id:2836,sort:'tk_rate'},function(data){
+    $.post('/goods/loadGoods',{pageNum:pageNum,pageSize:pageSize,material_id:material_id},function(data){
+      loading = false;
+      if(data.success){
+        var mapData = data.mapData;
+        if(mapData && mapData.length > 0){
+            $(mapData).each(function(){
+                $this = this;
+                // 天猫:icon-tianmao-18, 淘宝:icon-taobao-18, 京东:icon-jingdong-18, 拼多多:icon-pinduoduo-18
+                var tianmaoCss = 'icon-tianmao-18';
+                if($this.user_type == 0){
+                  tianmaoCss = 'icon-taobao-18';
+                }else if($this.user_type == 1){
+                  tianmaoCss = 'icon-tianmao-18';
+                }
+                var zkPrice = jsSubtr($this.zk_final_price,$this.coupon_amount);
+
+                // new图标是否显示，如果券开始时间为当天：显示，反之：隐藏
+                var newIcon = '<img src="'+appURL+'/images/flag-new-3.png" height="32" class="newFlag" style="display: block;">';
+                //if(typeof($this.coupon_start_time) != 'undefined'){
+                //  var coupon_start_time = $this.coupon_start_time;
+                //  if(coupon_start_time == timeFormat($this.createDate,'yyyy-MM-dd')){
+                //   newIcon = '<img src="'+appURL+'/images/flag-new-3.png" height="32" class="newFlag" style="display: block;">';
+                //  }
+                //}else{
+                //  newIcon = '';
+                //}
+
+                $('#goodsUL').append('<li>' +
+                      //'<a href="/goods/goodsDetail?numIid='+$this.item_id+'&platform='+platform+'">' +
+                      '<a href="javascript:void(0)" onclick="postGoodsDetail('+$this.item_id+','+platform+','+$this.commission_rate+','+$this.coupon_amount+',\'https:'+$this.coupon_click_url+'\',\'https:'+$this.click_url+'\',\''+$this.item_description+'\')">' +
+                        newIcon +
+                        '<span class="quanFlag"><div><b>'+$this.coupon_amount+'</b></div><div style="white-space:nowrap;color:#fff;">元券</div></span>' +
+                        '<div class="proimg">' +
+                          '<img src="http:'+$this.pict_url+'">' +
+                        '</div>' +
+                        '<div class="protxt">' +
+                        // 根据店铺类型判断是天猫、淘宝
+                          '<div class="name" style="position: relative;">' +
+                          '<i class="icon18-zz '+tianmaoCss+'" style="margin-right: 5px;"></i>' +
+                          '<em style="position: absolute;top: -1px;">'+$this.title+'</em></div>' +
+                          '<div class="name" style="position: relative;"><p>' +
+                            '<em class="nowPrice">￥'+zkPrice+'</em>' +
+                            '<span class="oldPrice">￥'+$this.zk_final_price+'</span>' +
+                            '<small class="monthOrderNum">月销'+$this.volume+'件</small>' +
+                            '</p></div>' +
+                          '<div class="return" style="margin-bottom: 3px;">' +
+                            '<div style="background-color: #dc2527;color: #fff;">标佣¥'+byFnc(zkPrice,$this.commission_rate)+'</div>' +
+                            '<div style="color: #dc2527;">特佣¥'+tyFnc(zkPrice,$this.commission_rate)+'</div>' +
+                            '</div>' +
+                          '</div>' +
+                        '</a>' +
+                      '</li>');
+            });
+        }else{
+            $("#listwrap").append("<div class=\"weui-cells__title\" style='text-align: center;'>已无更多数据</div>");
+            loading = true;
+        }
+      }else{
+        $("#listwrap").append("<div class=\"weui-cells__title\" style='text-align: center;'>已无更多数据</div>");
+        loading = true;
+        if(data.msg){
+          $.toast(data.msg, 'forbidden');
+        }else{
+          $.toast('发送失败', 'forbidden');
+        }
+      }
+
+      $(".weui-loadmore").hide();
+    });
+}
+// 查询页商品加载
+function loadGoodsForSearch(){
+  var platform = $('#platform').val();
+    $.post('/goods/loadSearchGoods',{pageNum:pageNum,pageSize:pageSize,q:$('#titleQuery').val(),material_id:$('#material_id').val(),has_coupon:$('#has_coupon').val(),sort:$('#sort').val()},function(data){
+      loading = false;
+      if(data.success){
+        var mapData = data.mapData;
+        if(mapData && mapData.length > 0){
+
+            var commission_rate,coupon_amount;
+            $(mapData).each(function(){
+                $this = this;
+                // 天猫:icon-tianmao-18, 淘宝:icon-taobao-18, 京东:icon-jingdong-18, 拼多多:icon-pinduoduo-18
+                var tianmaoCss = 'icon-tianmao-18';
+
+                commission_rate = 0;
+                coupon_amount = 0;
+
+                  commission_rate = $this.commission_rate / 100;
+                  if(!!$this.coupon_info && $this.coupon_info != ''){
+                    coupon_amount = formatCouponInfo($this.coupon_info);
+                  }
+
+                var zkPrice = jsSubtr($this.zk_final_price,coupon_amount);
+                $('#goodsUL').append('<li>' +
+                    //'<a href="/goods/goodsDetail?numIid='+num_iid+'&platform='+platform+'">' +
+                    '<a href="javascript:void(0)" onclick="postGoodsDetail('+$this.num_iid+','+platform+','+commission_rate+','+coupon_amount+',\'https:'+$this.coupon_share_url+'\',\'https:'+$this.url+'\',\'\')">' +
+                    '<img src="'+appURL+'/images/flag-new-3.png" height="32" class="newFlag" style="display: block;">' +
+                    '<span class="quanFlag"><b>'+coupon_amount+'</b><br>元券</span>' +
+                    '<div class="proimg">' +
+                    '<img src="'+$this.pict_url+'">' +
+                    '</div>' +
+                    '<div class="protxt">' +
+                    // 根据店铺类型判断是天猫、淘宝
+                    '<div class="name" style="position: relative;">' +
+                    '<i class="icon18-zz '+tianmaoCss+'" style="margin-right: 5px;"></i>' +
+                    '<em style="position: absolute;top: -1px;">'+$this.title+'</em></div>' +
+                    '<div class="name" style="position: relative;"><p>' +
+                    '<em class="nowPrice">￥'+zkPrice+'</em>' +
+                    '<span class="oldPrice">￥'+$this.zk_final_price+'</span>' +
+                    '<small class="monthOrderNum">月销'+$this.volume+'件</small>' +
+                    '</p></div>' +
+                    '<div class="return" style="margin-bottom: 3px;">' +
+                    '<div style="background-color: #d02c37;color: #fff;">标佣¥'+byFnc(zkPrice,commission_rate)+'</div>' +
+                    '<div style="color: #d02c37;">特佣¥'+tyFnc(zkPrice,commission_rate)+'</div>' +
+                    '</div>' +
+                    '</div>' +
+                    '</a>' +
+                    '</li>');
+            });
+        }else{
+            $("#goodsUL").append('<div class="weui-cells__title" style=\'text-align: center;\'>已无更多数据</div>');
+            loading = true;
+        }
+      }else{
+        $("#listwrap").append("<div class=\"weui-cells__title\" style='text-align: center;'>已无更多数据</div>");
+        loading = true;
+      }
+
+      $(".weui-loadmore").hide();
+    });
+}
+
+// 详情页加载商品
+function loadInfo(){
+    if(!!goodsDetail && goodsDetail.length > 0){
+      var item = goodsDetail[0];
+      numIid = item['num_iid'];
+      // 商品图片
+      pictUrl = item['pict_url'];
+      $('#picList').append('<div class="swiper-slide"><img src="'+pictUrl+'" /></div>');
+      var small_images = item['small_images'];
+      if(!!small_images && small_images['string'].length > 0){
+        for(var i = 0; i < small_images['string'].length; i++){
+          $('#picList').append('<div class="swiper-slide"><img src="'+small_images['string'][i]+'" /></div>');
+        }
+      }
+      // 券
+      $('#quanFlag').append('<b>￥'+coupon_amount+'</b>');
+      userType = item['user_type'];
+      if(userType == 0){// 淘宝图标
+        $('#itemTitle').append('<i class="icon18-zz icon-tianmao-18"></i>');
+      }else if(userType == 1){// 天猫图标
+        $('#itemTitle').append('<i class="icon18-zz icon-tianmao-18"></i>');
+      }
+      title = item['title'];
+      $('#itemTitle').append('<em style="padding:1.1rem;">'+title+'</em>');
+      // 原价、折扣价
+      zkFinalPrice = item['zk_final_price'];
+      zkPrice = jsSubtr(zkFinalPrice,coupon_amount);
+      $('#itemPrice').append('<em class="num" style="font-size: 0.95rem;font-weight: bold;">¥'+zkPrice+'</em>');
+      $('#itemPrice').append('<em class="detail-price-s">原价'+zkFinalPrice+'</em>');
+      volume = item['volume'];
+      $('#itemPrice').append('<em class="detail-price-v">月销量'+volume+'</em>');
+      // 包邮
+      if(!!item['free_shipment']){
+        freeShipment = item['free_shipment'];
+      }
+      // 佣金
+      $('#biaoYong').text(byFnc(zkPrice,commission_rate));
+      $('#teYong').text(tyFnc(zkPrice,commission_rate));
+      if(item_description != ''){
+        $('#itemDescription').text(item_description);
+      }else{
+        $('#itemDescription').css('text-align','center');
+      }
+      // 详情
+
+      // 评价
+
+    }
+}
+
+
+// 抢券淘口令
+function qiangquan(){
+    if(tkl == ''){
+        $.post('/goods/goodsTPwd',{url:coupon_click_url,text:title},function(data){
+            $.hideLoading();
+            if(data.success){
+                tkl = txt+data.model+'复制这条信息，打开手机淘宝即可领券';
+                $('#copyKeyAndroid').text(tkl);
+                $('#copyButton').attr('data-clipboard-text',tkl);
+            }else{
+                toast('');
+                if(data.msg){
+                  toast(data.msg);
+                }else{
+                  toast('获取淘口令失败');
+                }
+            }
+
+        });
+    }else{
+        showTKLWin(tkl);
+    }
+    showTKLWin();
+    showLoading('正在获取淘口令','style="min-width: 6.5rem;min-height: 6.5rem;"');
+}
+// 淘口令窗口
+function showTKLWin(){
+  var dialogId = 'qianquan';
+  var title = '<div style="background-color: #F54D23;color: #fff;font-size: 1rem;padding:0.25rem 0 0 0;line-height: 2.5rem;"><em>抢券淘口令</em><a style="position: absolute;right:0.25rem;top:0.2rem;font-size: 1.25rem;color:#E9E9E9;width:2rem;" id="'+dialogId+'Close" href="javascript:">X</a></div>';
+  var content = '<div id="copyDiv" class="puBody">' +
+        '<h2><em>长按框内 <u class="p3">&gt;</u> 全选 <u class="p3">&gt;</u> 复制</em></h2>' +
+        '<form name="puKoulingForm" id="puKoulingForm">' +
+          '<textarea class="inputArea inputArea-2" id="copyKeyAndroid"></textarea>' +
+        '</form>' +
+      '</div>' +
+      '<div id="copyButton" class="imne-copy-btn" style="display: block;" onclick="cloneTxt(\'copyButton\')">一键复制</div>' +
+      '<div class="hasCoupon" style="display: block;" id="hasCoupon">复制淘口令，打开【手机淘宝】即可领券！</div>';
+  var footer = '';
+  createDialog(dialogId,title,content,footer);
+  showDialog(dialogId);
+  // showLoading('正在获取淘口令','style="min-width: 6.5rem;min-height: 6.5rem;"');
+  // $.post('/goods/quanKey',{goodId:''},function (data){
+  //     $.hideLoading();
+  //     if(data && data.copyTxt){
+  //         $('#copyButton').attr('data-clipboard-text', data.copyTxt);
+  //         $('#copyKeyAndroid').text(data.copyTxt);
+  //         $('#copyButton').click(function (e) {
+  //             cloneTxt('copyButton');
+  //         })
+  //     }
+  //     showDialog(dialogId);
+  // });
+  // colseDialog(dialogId);
+}
+
+// 生成推广海报
+function haibao() {
+  if(!!goodsDetail && goodsDetail.length > 0){
+      var dialogId = 'haibao';
+      var len = $('#'+dialogId+'Win').length;
+      var content = '<div class="haibaoContent"><img id="goodsHaibao" onload="imgloadComplent(\''+dialogId+'\')"/></div><div class="haibao-c-remark" style="color: #d47b4f;">长按图片发送给朋友，或保存到手机</div><div class="haibao-c-remark" style="color: #d9d9db;">好友通过领券购买，您即可赚取佣金</div>';
+      var footer = '<div class="haibaoFoot"><a href="javascript:" id="copyTitle" onclick="cloneTxt(\'copyTitle\')" data-clipboard-text="'+title+'">复制标题</a><a href="javascript:" id="copyText" onclick="cloneTxt(\'copyText\')" data-clipboard-text="'+txt+'长按识别二维码，复制口伶或网址，打开手机淘宝，领券下单'+'">复制文案</a><a href="javascript:closeDialog(\''+dialogId+'\')">关闭</a></div>';
+      createDialog(dialogId,'',content,footer);
+      $.post('/goods/haibao',{
+          shopType : userType,
+          platform : platform,
+          numIid : numIid,
+          pictUrl : pictUrl,
+          title : title,
+          zkFinalPrice : zkFinalPrice,
+          reservePrice : reservePrice,
+          freeShipment : freeShipment,
+          volume : volume,
+          couponInfo : '0.00'//未确定
+      },function (data) {
+          $('#goodsHaibao').attr('src',data.url);
+      });
+      if(len > 0){
+          showDialog(dialogId);
+      }else{
+          //$.showLoading('正在生成<br>分享图片');// 自带的不能调节宽高
+          showLoading('正在生成海报','style="min-width: 7.5rem;min-height: 7.5rem;"');
+      }
+  }
+}
