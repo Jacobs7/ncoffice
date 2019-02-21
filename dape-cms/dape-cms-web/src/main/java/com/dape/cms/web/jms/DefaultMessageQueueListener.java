a68a5b032487857239a940e8ed3f02c1754cc5e3
DP <xushifu@live.cn>
2019-02-21 18:23:38 +0800

    修改公网连接

--- dape-cms/dape-cms-web/src/main/java/com/dape/cms/web/jms/DefaultMessageQueueListener.java
+++ dape-cms/dape-cms-web/src/main/java/com/dape/cms/web/jms/DefaultMessageQueueListener.java
@@ -12,29 +12,31 @@ import javax.jms.TextMessage;
 /**
  * MQ消费者
  * Created by ZhangShuzheng on 2016/11/24.
+ * 暂不启用
  */
 public class DefaultMessageQueueListener implements MessageListener {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DefaultMessageQueueListener.class);
 
-    @Autowired
-    ThreadPoolTaskExecutor threadPoolTaskExecutor;
+//    @Autowired
+//    ThreadPoolTaskExecutor threadPoolTaskExecutor;
 
     @Override
     public void onMessage(final Message message) {
-        // 使用线程池多线程处理
-        threadPoolTaskExecutor.execute(new Runnable() {
-            @Override
-            public void run() {
-                TextMessage textMessage = (TextMessage) message;
-                try {
-                    String text = textMessage.getText();
-                    LOGGER.info("消费：{}", text);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+//        // 使用线程池多线程处理
+//        threadPoolTaskExecutor.execute(new Runnable() {
+//            @Override
+//            public void run() {
+//                TextMessage textMessage = (TextMessage) message;
+//                try {
+//                    String text = textMessage.getText();
+//                    LOGGER.info("消费：{}", text);
+//                    System.out.println("消费：-->"+text);
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
     }
 
 }
