b4038846b3f544721e9155c3dd4aef42368e3558
DP <xushifu@live.cn>
2019-02-21 18:23:26 +0800

    修改公网连接

--- dape-cms/dape-cms-web/src/main/java/com/dape/cms/web/controller/CmsController.java
+++ dape-cms/dape-cms-web/src/main/java/com/dape/cms/web/controller/CmsController.java
@@ -1,30 +1,72 @@
 package com.dape.cms.web.controller;
 
+import com.alibaba.dubbo.config.support.Parameter;
+import com.alibaba.fastjson.JSON;
+import com.dape.cms.common.constant.CmsResult;
+import com.dape.cms.common.constant.CmsResultConstant;
 import com.dape.cms.dao.model.*;
+import com.dape.common.base.BaseController;
+import com.dape.common.util.JmsUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
+import javax.jms.Destination;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static com.dape.common.base.BaseController.thymeleaf;
 
 @Controller
 @RequestMapping(value = "/cms")
-public class CmsController {
+public class CmsController extends BaseController {
 
+    //MQ 消息发送者
+    @Autowired JmsTemplate jmsTemplate;
+
+    @Qualifier("defaultQueueDestination")
+    @Autowired Destination destination;
+
+    /**********************
+     * 用户登陆页面跳转
+     * ********************/
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String login(Model model) {
-
-
         return thymeleaf("/user/login");
     }
 
+    /**********************
+     * 用户注册页面跳转
+     * ********************/
     @RequestMapping(value = "reg", method = RequestMethod.GET)
     public String reg(Model model) {
-
-
         return thymeleaf("/user/reg");
+    }
+
+    /**********************
+     * 用户登陆验证
+     * ********************/
+    @RequestMapping(value = "/checkUserLogin", method = RequestMethod.POST)
+    @ResponseBody
+    public Object checkUserLogin(HttpServletRequest request, HttpServletResponse response, Model model){
+
+        String userName = request.getParameter("username");
+        String pwd = request.getParameter("password");
+
+        Map loginUser = new HashMap();
+        loginUser.put(userName,userName);
+        loginUser.put(pwd,pwd);
+
+        JmsUtil.sendMessage(jmsTemplate,destination, JSON.toJSONString(loginUser));
+
+        System.out.println("进入了Controller");
+        return new CmsResult(CmsResultConstant.SUCCESS, 0);
     }
 }
