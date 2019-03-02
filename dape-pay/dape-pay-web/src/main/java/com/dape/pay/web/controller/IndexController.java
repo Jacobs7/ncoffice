2f724d937f5df2a9c79b696bbf26121c907c2a0d
ty3277 <327734933@163.com>
2019-03-02 18:12:48 +0800

    调试项目

--- dape-pay/dape-pay-web/src/main/java/com/dape/pay/web/controller/IndexController.java
+++ dape-pay/dape-pay-web/src/main/java/com/dape/pay/web/controller/IndexController.java
@@ -1,13 +1,25 @@
 package com.dape.pay.web.controller;
 
 
+import com.alibaba.fastjson.JSON;
 import com.dape.common.base.BaseController;
+import com.dape.common.util.JmsUtil;
+import com.dape.pay.common.constant.PayResult;
+import com.dape.pay.common.constant.PayResultConstant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.jms.Destination;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 支付宝
@@ -16,9 +28,17 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class IndexController extends BaseController {
 
+    //MQ 消息发送者
+    @Autowired
+    JmsTemplate jmsTemplate;
+
+    @Qualifier("defaultQueueDestination")
+    @Autowired
+    Destination destination;
+
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String index(Model model, HttpServletRequest request) {
-
         return thymeleaf("/index");
     }
+
 }
--- dape-pay/dape-pay-web/src/main/java/com/dape/pay/web/interceptor/PayWebInterceptor.java
+++ dape-pay/dape-pay-web/src/main/java/com/dape/pay/web/interceptor/PayWebInterceptor.java
@@ -1,6 +1,8 @@
 package com.dape.pay.web.interceptor;
 
 import com.dape.common.util.PropertiesFileUtil;
+import com.dape.common.util.RedisUtil;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,13 +15,32 @@ public class PayWebInterceptor extends HandlerInterceptorAdapter {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PayWebInterceptor.class);
 
+    private final static String DAPE_UPMS_SERVER_SESSION_ID = "dape-upms-server-session-id";
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        String hasCode = RedisUtil.get(DAPE_UPMS_SERVER_SESSION_ID + "_" + request.getSession().getId().toString());
+
+        String url = request.getRequestURI();
+        if(url.startsWith("/login")){// 排除登录相关请求
+            System.out.println("登录相关请求");
+            if(StringUtils.isNotBlank(hasCode)){// 如果登录重定向到首页
+                response.sendRedirect("/");// 重定向到登录页
+            }
+        }else{// 非登录相关，验证登录状态
+            System.out.println("业务相关请求");
+            if(StringUtils.isBlank(hasCode)){// 未登录重定向到登录页
+                response.sendRedirect("/login");// 重定向到登录页
+            }
+        }
 
         // 过滤ajax
         if (null != request.getHeader("X-Requested-With") && "XMLHttpRequest".equalsIgnoreCase(request.getHeader("X-Requested-With"))) {
             return true;
         }
+
+
 
         // zheng-ui静态资源配置信息
         String appName = PropertiesFileUtil.getInstance().get("app.name");
