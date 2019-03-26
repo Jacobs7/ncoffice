113baa044a3c7767bc097a0440ff27756b49960d
ty3277 <327734933@163.com>
2019-03-26 23:04:55 +0800

    bc工程提交

---
+++ dape-bc/dape-bc-rpc-service/src/main/java/com/dape/bc/rpc/DapeBcRpcServiceApplication.java
@@ -0,0 +1,19 @@
+package com.dape.bc.rpc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * 服务启动类
+ * Created by TengYong on 2018/11/17.
+ */
+public class DapeBcRpcServiceApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DapeBcRpcServiceApplication.class);
+
+    public static void main(String[] args) {
+        LOGGER.info(">>>>> dape-shop-rpc-service 正在启动 <<<<<");
+        new ClassPathXmlApplicationContext("classpath:META-INF/spring/*.xml");
+        LOGGER.info(">>>>> dape-shop-rpc-service 启动完成 <<<<<");
+    }
+}
